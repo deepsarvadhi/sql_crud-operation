796fb7dec4909d92f0e7a51650ba756221e26f54
deepsarvadhi <deep.sutariya@sarvadhi.com>
2023-06-09 11:38:29 +0530

    update

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const app = express();
 const mysql = require('mysql');
 const userrouter = require('./app/router/user.roter');
 const adminrouter = require('./app/router/admin.router');
+const cors = require('cors');
 
 require('./config/connection');
 
 app.use(express.json());
+
+app.use(cors({ credentials: true, origin: true }));
+
 app.use(userrouter);
 app.use(adminrouter);
+
+app.all('*', (req, res) => {
+    return res.status(404).send("URL not found server.js")
+})
 
 app.listen(5001, (err) => {
     console.log('Server is running on port 5001');

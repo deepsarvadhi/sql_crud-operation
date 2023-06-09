b108a5b8db38267e9fd77d4287fd1173b21d0cea
deepsarvadhi <deep.sutariya@sarvadhi.com>
2023-06-09 10:10:06 +0530

    update

--- app/controller/admin.controller.js
+++ app/controller/admin.controller.js
@@ -3,54 +3,9 @@ const mysql = require('mysql');
 const connection = require('../../config/connection');
 const HTTP = require('../../constants/responseCode.constant');
 
-// (async function deafultAdminsignup(req, res) {
-//     try {
-//       const adminData = {
-//         username: "admin",
-//         email: "larosa.admin@yopmail.com",
-//         role: "admin",
-//       };
-//       const password = "larosaadmin@123";
-//       // const encData = await encryptUserModel(adminData)
-//       const existsAdmin = await Register.findOne({
-//         email: adminData.email,
-//         role: adminData.role,
-//       });
-
-//       //Admin exist
-//       if (existsAdmin) return;
-
-//       const userData = await new Register({
-//         ...adminData,
-//         password: hashSync(password.trim(), 8),
-//         isVerified: true,
-//       }).save();
-//       if (!userData) console.log("Unable to add default admin");
-
-//       return;
-//     } catch (e) {
-//       console.log(e);
-//       return;
-//     }
-//   })();
-
 
 (async function deafultAdminsignup(req, res) {
     try {
-        // var sql = `INSERT INTO 'admin' ( 'name', 'email', 'password', 'role') VALUES ('Admin','Admin@gmail.com','Admin@123','Admin')`;
-        var sql = `INSERT INTO admin (name, email, password, role) VALUES ('Admin', 'Admin@gmail.com', 'Admin@123', 'Admin')`;
-
-        connection.query(sql, (err, data) => {
-            if (err) {
-                console.log("🚀 ~ file: user.controller.js:18 ~ connection.query ~ err:", err);
-            }
-
-            if (!data) {
-                return false;
-            }
-            return true;
-        });
-
         //Admin exist
         var sqlcheck = `SELECT * FROM admin WHERE email = 'Admin@gmail.com'`
 
@@ -64,6 +19,22 @@ const HTTP = require('../../constants/responseCode.constant');
             }
             return true;
         });
+
+        //Default admin insert
+
+        var sql = `INSERT INTO admin (name, email, password, role) VALUES ('Admin', 'Admin@gmail.com', 'Admin@123', 'Admin')`;
+
+        connection.query(sql, (err, data) => {
+            if (err) {
+                console.log("🚀 ~ file: user.controller.js:18 ~ connection.query ~ err:", err);
+            }
+
+            if (!data) {
+                return true;
+            }
+            return false;
+        });
+
 
     } catch (error) {
         console.log("🚀 ~ file: admin.controller.js:39 ~ deafultAdminsignup ~ error:", error)

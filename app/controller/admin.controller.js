7b643ed58e14ffbc16577c3a1622c33d72b28eeb
deepsarvadhi <deep.sutariya@sarvadhi.com>
2023-06-09 10:53:52 +0530

    update

--- app/controller/admin.controller.js
+++ app/controller/admin.controller.js
@@ -2,27 +2,49 @@ const express = require('express');
 const mysql = require('mysql');
 const connection = require('../../config/connection');
 const HTTP = require('../../constants/responseCode.constant');
+var jwt = require('jsonwebtoken');
 
+// Default Sign up
 
 (async function deafultAdminsignup(req, res) {
     try {
         //Admin exist
-        var sqlcheck = `SELECT * FROM admin WHERE email = 'Admin@gmail.com'`
+        var sqlcheck = `SELECT * FROM admin WHERE email = 'Admin@gmail.com'`;
 
         connection.query(sqlcheck, (err, data) => {
             if (err) {
                 console.log("🚀 ~ file: user.controller.js:18 ~ connection.query ~ err:", err);
             }
+            if (!data == " ") return;
 
-            if (data) {
+            //Default admin insert
+
+            var sql = `INSERT INTO admin (name, email, password, role) VALUES ('Admin', 'Admin@gmail.com', 'Admin@123', 'Admin')`;
+
+            connection.query(sql, (err, data) => {
+                if (err) {
+                    console.log("🚀 ~ file: user.controller.js:18 ~ connection.query ~ err:", err);
+                }
+
+                if (!data) {
+                    return false;
+                }
                 return false;
-            }
-            return true;
+            });
+
         });
 
-        //Default admin insert
+    } catch (error) {
+        console.log("🚀 ~ file: admin.controller.js:39 ~ deafultAdminsignup ~ error:", error)
+    }
+})();
 
-        var sql = `INSERT INTO admin (name, email, password, role) VALUES ('Admin', 'Admin@gmail.com', 'Admin@123', 'Admin')`;
+// Sign in
+
+async function adminSignIn(req, res) {
+    try {
+        let { email, password } = req.body;
+        let sql = `SELECT * FROM admin WHERE email = '${email}' AND password = '${password}'`;
 
         connection.query(sql, (err, data) => {
             if (err) {
@@ -30,13 +52,20 @@ const HTTP = require('../../constants/responseCode.constant');
             }
 
             if (!data) {
-                return true;
+                return res.status(HTTP.SUCCESS).send({ status: false, code: HTTP.BAD_REQUEST, message: "Email or Password Increct", data: {} });
             }
-            return false;
+
+            const token = jwt.sign({ email: data.email, id: data.id, role: data.role }, 'admin@123');
+
+            return res.status(HTTP.SUCCESS).send({ status: true, code: HTTP.SUCCESS, message: "Admin Sign In", data: token });
+
         });
 
-
     } catch (error) {
-        console.log("🚀 ~ file: admin.controller.js:39 ~ deafultAdminsignup ~ error:", error)
+        console.log("🚀 ~ file: admin.controller.js:48 ~ adminSignIn ~ error:", error)
     }
-})();
+}
+
+module.exports = {
+    adminSignIn
+}
--- app/router/admin.router.js
+++ app/router/admin.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const admincontroller = require('../controller/admin.controller');
 
-
+router.post('/admin_signIn', admincontroller.adminSignIn)
 
 
 
